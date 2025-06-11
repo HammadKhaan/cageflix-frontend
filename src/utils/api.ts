83f20d0d0b1d3248724198ce67f533fbb0747a01
unknown <hammadkhan.1199@gmail.com>
2025-06-11 11:36:21 +0200

    misc: environment based url added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,7 +7,7 @@ export type Movie = {
   rating: number;
 };
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // Helper to wrap fetch with error handling
 async function fetchWithHandling<T>(url: string): Promise<T> {
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cageflix-backend-9fb8.onrender.com',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
