6bdee3696e2dcc8ae81650237fffbf621502eaa4
unknown <hammadkhan.1199@gmail.com>
2025-06-18 03:29:22 +0200

    ref: removed unused variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-// main.tsx or index.tsx
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import "./index.css";
 
